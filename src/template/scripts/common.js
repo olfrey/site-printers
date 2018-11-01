4b3c2de8c6ea480ef903f65d6cc8d0573f9a3062
Oleg Frey <olfrey@ya.ru>
2018-11-02 01:59:05 +0300

    full

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,33 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+	var panel = $('.panel'),
+			panelClass = 'panel_scroll';
+
+	$(window).on('load scroll', function () {
+		var scrollTop = $(window).scrollTop();
+
+		if (scrollTop > 1) {
+			panel.addClass(panelClass);
+		} else {
+			panel.removeClass(panelClass);
+		}
+	});
+
+	var reviews = $('.reviews__list');
+	reviews.slick({
+		slidesToShow: 4,
+		responsive: [{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3
+			}}, {
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 1
+			}
+		}]
+	})
+
+	new WOW().init();
 });
